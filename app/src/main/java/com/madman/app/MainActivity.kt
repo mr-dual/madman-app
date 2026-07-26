7acc7bc8c95d3a7f312233891b0cf139bed3db6d
Mr Dual <kaziuzan273@gmail.com>
2026-07-26 14:26:26 +0600

    chore: some dependancy stuff and add a bit of docs.

--- app/src/main/java/com/madman/app/MainActivity.kt
+++ app/src/main/java/com/madman/app/MainActivity.kt
@@ -2,17 +2,14 @@ package com.madman.app
 
 import android.os.Bundle
 import android.app.Activity
-import com.madman.engine.EngineRenderView // 1. We import our engine UI component
+import com.madman.engine.EngineRenderView 
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // 2. We instantly instantiate our custom engine view using the current screen context
         val renderView = EngineRenderView(this)
         
-        // 3. Instead of loading a text file or standard XML UI, we tell the phone: 
-        // "Make this Vulkan rendering surface fill the entire user's screen."
         setContentView(renderView)
     }
 }
--- engine/src/main/java/com/madman/engine/MadmanJni.kt
+++ engine/src/main/java/com/madman/engine/MadmanJni.kt
@@ -9,8 +9,20 @@ class MadmanJni {
         }
     }
 
+    /** Initialize the renderer and loop
+     * @param surface the android Surface view 
+     **/
     external fun initialize(surface: Surface)
+
+    /**Called when thw screen is resized
+     * @param width the width of the new view
+     * @param height the height of the new view
+     **/
     external fun resize(width: Int, height: Int)
+
+    /**
+     * called when the screen is closed
+     **/
     external fun terminate()
 }
 
